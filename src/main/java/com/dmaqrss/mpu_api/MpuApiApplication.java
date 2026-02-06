7cc800b10d870479bd06075e7114ce9d2e710308
dmarqss <davimarques864@gmail.com>
2026-02-06 02:30:43 -0300

    chore: initial project setup

---
+++ src/main/java/com/dmaqrss/mpu_api/MpuApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmaqrss.mpu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmaqrss/mpu_api/MpuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmaqrss.mpu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
