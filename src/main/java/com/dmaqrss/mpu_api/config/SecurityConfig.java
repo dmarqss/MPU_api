d9cfcf4d390012a3828f5295c64637a9e0cfeb2f
dmarqss <davimarques864@gmail.com>
2026-03-29 22:11:52 -0300

    chore: update docker-compose and create dockerfile

--- src/main/java/com/dmaqrss/mpu_api/config/SecurityConfig.java
+++ src/main/java/com/dmaqrss/mpu_api/config/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +28,7 @@ public class SecurityConfig {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/swagger-ui/**","/v3/api-docs/**", "/swegger-ui.html").permitAll()
+                        .requestMatchers( "/swagger-ui/*/**","/v3/api-docs/**", "/swegger-ui.html").permitAll()
 
                         .requestMatchers(HttpMethod.POST, "/products").hasRole("SELLER")
                         .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("SELLER")
