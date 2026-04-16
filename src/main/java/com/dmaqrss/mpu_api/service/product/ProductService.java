0468c55a66f7ceb6f6dcfa07d8bedf71c723de47
dmarqss <davimarques864@gmail.com>
2026-04-16 10:52:52 -0300

    feat[products]: logs implementation

--- src/main/java/com/dmaqrss/mpu_api/service/product/ProductService.java
+++ src/main/java/com/dmaqrss/mpu_api/service/product/ProductService.java
@@ -7,6 +7,7 @@ import com.dmaqrss.mpu_api.exception.BusinessException;
 import com.dmaqrss.mpu_api.mapper.ProductMapper;
 import com.dmaqrss.mpu_api.model.Product;
 import com.dmaqrss.mpu_api.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 
+@Slf4j
 @Service
 public class ProductService {
     @Autowired
@@ -25,35 +27,67 @@ public class ProductService {
 
     @CacheEvict(value = {"products", "listProducts"}, allEntries = true)
     public ProductResponseDTO create(ProductRequestDTO dto){
+        log.info("[PRODUCT] Solicitação criação de novo produto: {}", dto.barCode());
+
         if(repository.existsByBarCode(dto.barCode())){
+            log.warn("[PRODUCT] Tentativa de registo com codigo de barras ja existente: {}", dto.barCode());
             throw new BusinessException("o codigo de barras ja existe");
         }
+
         Product saved = repository.save(mapper.toEntity(dto));
+        log.info("[PRODUCT] Produto criado com sucesso: {}", dto.barCode());
         return mapper.toResponse(saved);
     }
 
     @CacheEvict(value = {"products", "listProducts"}, allEntries = true)
     public void delete(Long barCode){
-        Product product = repository.findByBarCode(barCode).orElseThrow(() -> new BusinessException("o codigo de barras nao existe"));
+        log.info("[PRODUCT] Solicitação delete de produto: {}", barCode);
+
+        Product product = repository.findByBarCode(barCode).orElseThrow(() -> {
+            log.warn("[PRODUCT] tentativa de deletar produto inexistente: {}", barCode);
+            return new BusinessException("o codigo de barras nao existe");
+        });
+
         repository.delete(product);
+        log.info("[PRODUCT] Produto deletado com sucesso: {}", product.getBarCode());
     }
 
     @CacheEvict(value = {"products", "listProducts"}, allEntries = true)
     public ProductResponseDTO update(ProductUpdateDTO dto, Long barCode){
-        Product product = repository.findByBarCode(barCode).orElseThrow(() -> new BusinessException("o codigo de barras nao existe"));
+        log.info("[PRODUCT] Solicitação update do produto: {}", barCode);
+
+        Product product = repository.findByBarCode(barCode).orElseThrow(() -> {
+            log.warn("[PRODUCT] Tentativa de update de produto inexistente: {}", barCode);
+            return new BusinessException("o codigo de barras nao existe");
+        });
+
         BeanUtils.copyProperties(dto, product);
         repository.save(product);
+        log.info("[PRODUCT] Update no produto com sucesso: {}", product.getBarCode());
         return mapper.toResponse(product);
     }
 
     @Cacheable(value = "products", key = "#barCode")
     public ProductResponseDTO getProduct(Long barCode){
-        Product product = repository.findByBarCode(barCode).orElseThrow(() -> new BusinessException("o codigo de barras nao existe"));
+        log.info("[PRODUCT] Solicitação get do produto: {}", barCode);
+
+        Product product = repository.findByBarCode(barCode).orElseThrow(() -> {
+            log.warn("[PRODUCT] Tentativa de retorno de produto inexistente: {}", barCode);
+            return new BusinessException("o codigo de barras nao existe");
+        });
+
+        log.info("[PRODUCT] Retorno do produto com sucesso: {}", product.getBarCode());
         return mapper.toResponse(product);
     }
 
     @Cacheable(value = "listProducts", key = "#pageable.pageNumber + '-' + #pageable.pageSize + '-' + #pageable.sort.toString()")
     public Page<ProductResponseDTO> getProducts(Pageable pageable){
-        return repository.findAll(pageable).map(mapper::toResponse);
+        log.info("[PRODUCT] Solicitação paginação de produtos: page={}, size={}",
+                pageable.getPageNumber(), pageable.getPageSize());
+
+        Page<ProductResponseDTO> result = repository.findAll(pageable).map(mapper::toResponse);
+
+        log.info("[PRODUCT] Retornando {} produtos", result.getTotalElements());
+        return result;
     }
 }
